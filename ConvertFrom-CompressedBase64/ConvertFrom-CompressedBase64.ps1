955222d6f4abf9b870d5cb62a1f8b19c94422e6d
Jaap Brasser <noreply@jaapbrasser.com>
2016-12-21 15:14:12 +0100

    Added CompressedBase64 functions

---
+++ ConvertFrom-CompressedBase64/ConvertFrom-CompressedBase64.ps1
@@ -0,0 +1,16 @@
+﻿Function ConvertFrom-CompressedBase64 {
+    [cmdletbinding()]
+    param(
+        [Parameter(
+            ValueFromPipeline=$true
+        )]
+        [string] $InputObject
+    )
+    $binarydata = [System.Convert]::FromBase64String($InputObject)
+    $ms = New-Object System.IO.MemoryStream
+    $ms.Write($binaryData, 0, $binaryData.Length)
+    $null = $ms.Seek(0,0)
+    $cs = New-Object System.IO.Compression.GZipStream($ms, [System.IO.Compression.CompressionMode]"Decompress")
+    $sr = New-Object System.IO.StreamReader($cs)
+    $sr.ReadToEnd()    
+}
---
+++ ConvertTo-CompressedBase64/ConvertTo-CompressedBase64.ps1
@@ -0,0 +1,15 @@
+﻿Function ConvertTo-CompressedBase64 {
+    [cmdletbinding()]
+    param(
+        [Parameter(
+            ValueFromPipeline=$true
+        )]
+        [string] $InputObject
+    )
+    $ms = New-Object System.IO.MemoryStream
+    $cs = New-Object System.IO.Compression.GZipStream($ms, [System.IO.Compression.CompressionMode]::Compress)
+    $sw = New-Object System.IO.StreamWriter($cs)
+    $sw.Write($InputObject.ToCharArray())
+    $sw.Close()
+    [System.Convert]::ToBase64String($ms.ToArray())
+}
