6333eb157f4663058c3f54a6483b2c98004dabe3
Jaap Brasser <noreply@jaapbrasser.com>
2017-09-07 11:43:13 +0200

    Added PowerShell Gallery version of script

---
+++ ConvertFrom-EncodedUrl/PowerShellGallery/Scriptinfo.ps1
@@ -0,0 +1,41 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 58101b69-ce79-49ac-b675-890ad1932ba7
+
+.AUTHOR Jaap Brasser
+
+.COMPANYNAME PowerShell Community
+
+.COPYRIGHT 
+
+.TAGS PowerShell Percent Encoding Obfuscation PowerShell Url Encoding Security
+
+.LICENSEURI 
+
+.PROJECTURI https://github.com/jaapbrasser/SharedScripts/tree/master/ConvertFrom-EncodedUrl
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ This function can be used to extract Percent-Encoded information from urls and display that information and return that as a PowerShell custom object. This function is pipeline aware and urls can be piped directly into this function. 
+
+#> 
+Param()
+
+
