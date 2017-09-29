c0172680ade5f0e972be189c3fccf8417d966b09
Jaap Brasser <noreply@jaapbrasser.com>
2017-09-29 19:21:40 +0900

    Updated help and functionality of New-ZeroFile

--- New-ZeroFile/New-ZeroFile.ps1
+++ New-ZeroFile/New-ZeroFile.ps1
@@ -1,40 +1,45 @@
 function New-ZeroFile {
 <#
 .SYNOPSIS
-Retrieves Storage Sense options in Windows 10
+Creates an empty, zeroed out file
 
 .DESCRIPTION 
-This function can retrieve Storage Sense options in Windows 10
-
-.PARAMETER Force
-Overwrites an existing file if the specified path already exists
+This function serves as a wrapper for fsutil, and can be used to create an empty file to reserve disk space
 
 .PARAMETER Path
 The path and file name of the zero file that will be created
 
+.PARAMETER FileSize
+Specifies the size, in bytes,  of the file, 1KB / 1MB / 1GB / 1TB notatation can be used 
 
+.PARAMETER Force
+Overwrites an existing file if the specified path already exists
 
-.NOTES   
+.OTES   
 Name:        New-ZeroFile
 Author:      Jaap Brasser
 DateCreated: 2017-01-26
-DateUpdated: 2017-01-26
-Version:     1.0.0
+DateUpdated: 2017-09-29
+Version:     1.1.0
 Blog:        http://www.jaapbrasser.com
 
 .LINK
 http://www.jaapbrasser.com
 
 .EXAMPLE   
-New-ZeroFile
+New-ZeroFile -Path c:\temp\jb2.file -FileSize 500mb
 
 Description
 -----------
-Retrieves all storage sense configuration and recently cleaned data from the current system
+Creates a 500MB file in C:\Temp named jb2.file
 #>  
 
     param(
-
+        [string] $Path,
+        [alias('Size')]
+        [long]   $FileSize,
+        [switch] $Force
     )
 
+    fsutil.exe file createnew ""$Path"" $FileSize
 }
