4b3863156937893f243834fbed893ba620d7a0e5
Jaap Brasser <noreply@jaapbrasser.com>
2017-10-01 23:59:46 +0900

    Added Get-WifiProfile script

---
+++ Get-WifiProfile/Get-WifiProfile.ps1
@@ -0,0 +1,8 @@
+﻿(netsh wlan show profile) -match ' : ' | ForEach-Object {
+    $Hash = [ordered]@{
+        WifiName = ($_ -split '\:',2)[1].Trim()
+    }
+    $Hash.Password = (-join ((netsh wlan show profile "$($Hash.WifiName)" key=clear) -match 'key c')) -replace '\s*Key Content\s+: '
+
+    [pscustomobject]$Hash
+}
