ca75b59fc2c9c964d2096a1713d5f9bc0a63bbda
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-09 16:59:33 +0200

    Added comment-based help

--- ConvertTo-Base64GUIExample/ConvertTo-Base64GUIExample.ps1
+++ ConvertTo-Base64GUIExample/ConvertTo-Base64GUIExample.ps1
@@ -1,4 +1,14 @@
-[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
-$InputBox = [Microsoft.VisualBasic.Interaction]::InputBox("Let's convert this to base64", "Sabrina's App", $null)
-$InputBox = [convert]::ToBase64String([char[]]$InputBox)
-[Microsoft.VisualBasic.Interaction]::MsgBox($InputBox,0,"Sabrina's App")
+function ConvertTo-Base64GUIExample {
+<#
+.SYNOPSIS
+Function to showcase some of the PowerShell GUI capabilities
+
+.DESCRIPTION
+This function contains various examples of using the GUI capabilities of both Windows PowerShell and PowerShell (Core). This is inteded to be used as a reference for those interested in building basic GUIs with PowerShell
+#>
+
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+    $InputBox = [Microsoft.VisualBasic.Interaction]::InputBox("Let's convert this to base64", "Sabrina's App", $null)
+    $InputBox = [convert]::ToBase64String([char[]]$InputBox)
+    [Microsoft.VisualBasic.Interaction]::MsgBox($InputBox,0,"Sabrina's App")
+}
