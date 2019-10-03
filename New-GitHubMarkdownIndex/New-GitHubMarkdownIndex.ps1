bf54f7671992534842454d53dd79f4d92b625060
Jaap Brasser <noreply@jaapbrasser.com>
2019-10-03 15:23:48 -0600

    Automated count

--- New-GitHubMarkdownIndex/New-GitHubMarkdownIndex.ps1
+++ New-GitHubMarkdownIndex/New-GitHubMarkdownIndex.ps1
@@ -3,11 +3,11 @@ function New-GitHubMarkdownIndex {
         $Path = 'C:\Temp\Events',
         $GitHubUri = 'https://github.com/jaapbrasser/events/tree/master'
     )
-    get-childitem -LiteralPath $Path | % {
+    Get-ChildItem -LiteralPath $Path | % {
         $GHPath = $_.FullName -replace [regex]::Escape($Path) -replace '\\','/' -replace '\s','%20'
         "* [$(Split-Path $_ -Leaf)]($GitHubUri$GHPath)"
         $_ | ls -recurse | ? {$_.PSIsContainer -or $_.Extension -eq '.md'} | select -exp fullname | % {
-            $Count = ($_ -split '\\').Count -4
+            $Count = ($_ -split '\\').Count-($Path.Split('\').Count+1)
             $GHPath = $_ -replace [regex]::Escape($Path) -replace '\\','/' -replace '\s','%20'
             "$(" "*$Count*2)* [$(Split-Path $_ -Leaf)]($GitHubUri$GHPath)"
         }
